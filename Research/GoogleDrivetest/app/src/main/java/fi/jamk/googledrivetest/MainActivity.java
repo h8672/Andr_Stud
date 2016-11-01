84c0735ed79375863d1d6e04a00904f8fd1c8a0e
Juha-Matti Kokkonen <H8672@LABRANET.JAMK.FI>
2016-11-01 14:55:55 +0200

    Reading Drive

--- Research/GoogleDrivetest/app/src/main/java/fi/jamk/googledrivetest/MainActivity.java
+++ Research/GoogleDrivetest/app/src/main/java/fi/jamk/googledrivetest/MainActivity.java
@@ -6,8 +6,10 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -18,9 +20,19 @@ import com.google.android.gms.drive.Drive;
 import com.google.android.gms.drive.DriveApi;
 import com.google.android.gms.drive.DriveContents;
 import com.google.android.gms.drive.DriveFile;
+import com.google.android.gms.drive.DriveFolder;
+import com.google.android.gms.drive.Metadata;
+import com.google.android.gms.drive.MetadataBuffer;
+import com.google.android.gms.drive.MetadataChangeSet;
 import com.google.android.gms.drive.query.Filters;
 import com.google.android.gms.drive.query.Query;
 import com.google.android.gms.drive.query.SearchableField;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity implements
@@ -129,16 +141,70 @@ public class MainActivity extends AppCompatActivity implements
     Query query = new Query.Builder()
             .addFilter(Filters.eq(SearchableField.TITLE, filename))
             .build();
+
+        //Creating folder to root folder in Google Drive
+        DriveFolder folder;
+        folder = Drive.DriveApi.getRootFolder(GAC);
+        MetadataChangeSet set = new MetadataChangeSet.Builder()
+                .setTitle("New folder").build();
+        //Callback class
+        ResultCallback<DriveFolder.DriveFolderResult> folderCreatedCallback = new
+                ResultCallback<DriveFolder.DriveFolderResult>() {
+                    @Override
+                    public void onResult(DriveFolder.DriveFolderResult result) {
+                        if (!result.getStatus().isSuccess()) {
+                            yell("Error while trying to create the folder");
+                            return;
+                        }
+                        yell("Created a folder: " + result.getDriveFolder().getDriveId());
+                    }
+                };
+        //Create folder with callback
+        folder.createFolder(GAC, set).setResultCallback(folderCreatedCallback);
+        //DriveContents contents;
+        ResultCallback<DriveApi.MetadataBufferResult> listContents = new ResultCallback<DriveApi.MetadataBufferResult>() {
+            @Override
+            public void onResult(@NonNull DriveApi.MetadataBufferResult result) {
+                if(!result.getStatus().isSuccess()){
+                    yell("Error while trying to get contents");
+                    return;
+                }
+                MetadataBuffer data = result.getMetadataBuffer();
+                Log.wtf("Count", ""+data.getCount());
+                ArrayList list = new ArrayList();
+                for (Metadata m : data){
+                    //Check if it is folder or extension file with different types
+                    if(m.isFolder()) list.add("Folder"); //Picture
+                    else list.add(m.getFileExtension());
+                    list.add(m.getTitle());
+                    list.add(m.getCreatedDate());
+                    list.add(m.getFileSize());
+                    list.add(m.getMimeType());
+                }
+                ShowFileList(list);
+            }
+        };
+        folder.listChildren(GAC).setResultCallback(listContents);
+        //yell();
+        /*folder.queryChildren(GAC, query).setResultCallback(new ResultCallback<DriveApi.MetadataBufferResult>() {
+            @Override
+            public void onResult(@NonNull DriveApi.MetadataBufferResult result) {
+                int items = result.getMetadataBuffer().getCount();
+                yell("Containts: " + items + " items and string: " + result.toString());
+            }
+        });*/
+        //Drive.DriveApi.fetchDriveId(GAC, "");
     // Invoke the query asynchronously with a callback method
     Drive.DriveApi.query(GAC, query)
             .setResultCallback(new ResultCallback<DriveApi.MetadataBufferResult>() {
                 @Override
                 public void onResult(DriveApi.MetadataBufferResult result) {
+                    if(result.getStatus().isSuccess()) return;
                     // Success! Handle the query result.
                     //TODO handling query results! But how is it handled?
-                    int items = result.getMetadataBuffer().getCount();
 
-                    yell("Hohoi! Täällä on jotain! "+items+" kpl, "+result.toString());
+
+                    yell("Hohoi! Täällä on jotain! "+" kpl, "+result.toString());
                 }
             });
 
@@ -147,5 +213,11 @@ public class MainActivity extends AppCompatActivity implements
     //Just want to yell!
     private void yell(String str){
         Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
+    }
+
+    private void ShowFileList(ArrayList array){
+        TextView textView = (TextView) findViewById(R.id.txtFilename);
+        String str = "";
+        textView.setText(array.toString());
     }
 }
