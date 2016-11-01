0958034a051441a79c76dc49f20d9e6776711266
Juha-Matti Kokkonen <H8672@LABRANET.JAMK.FI>
2016-11-01 12:51:03 +0200

    Fragments
    Fragments were introduced in course, so we did example of it.

---
+++ FragmentExample/app/src/main/java/com/example/h8672/fragmentexample/SomeFragment.java
@@ -0,0 +1,27 @@
+package com.example.h8672.fragmentexample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by H8672 on 1.11.2016.
+ */
+
+public class SomeFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_some, container, false);
+        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+        textView.setText("Some Fragment used!");
+        return rootView;
+
+        //Default
+        //return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
